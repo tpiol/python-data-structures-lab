2a599fd24bae93edcbc96b85f797240fd794ac09
Tia Piol <tiarox02@yahoo.com>
2025-09-04 09:44:57 -0700

    exercise 1

--- exercises.py
+++ exercises.py
@@ -15,3 +15,17 @@ def example_list_function():
 
 # Call the function and print each element
 example_list_function()
+
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+def manage_students():
+   students = ['tia', 'peri', 'randall']
+   _, first_student, last_student = students
+   return first_student, last_student
+
+# Call the function and print the result
+print('Exercise 1:', manage_students())
